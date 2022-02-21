fc1e4eac9e3790956f4b31cff965f08db8478a5d
daniehei <daniel.heimgartner@ivt.baug.ethz.ch>
2022-02-21 10:15:21 +0100

    exported functions (namespace)

--- R/api.R
+++ R/api.R
@@ -1,9 +1,11 @@
+#' @export
 locale <- function(id, locale) {
   method <- "locale"
   callJS()
 }
 
 
+#' @export
 showNavigationButtons <- function(id, showNavigationButtons = c("bottom", "top", "both", "none")) {
   showNavigationButtons <- match.arg(showNavigationButtons)
   method <- "showNavigationButtons"
@@ -11,6 +13,7 @@ showNavigationButtons <- function(id, showNavigationButtons = c("bottom", "top",
 }
 
 
+#' @export
 showQuestionNumbers <- function(id, showQuestionNumbers = c("on", "onpage", "off")) {
   showQuestionNumbers <- match.arg(showQuestionNumbers)
   method <- "showQuestionNumbers"
@@ -18,6 +21,7 @@ showQuestionNumbers <- function(id, showQuestionNumbers = c("on", "onpage", "off
 }
 
 
+#' @export
 showProgressBar <- function(id, showProgressBar = c("off", "top", "bottom", "both")) {
   showProgressBar <- match.arg(showProgressBar)
   method <- "showProgressBar"
@@ -25,78 +29,91 @@ showProgressBar <- function(id, showProgressBar = c("off", "top", "bottom", "bot
 }
 
 
+#' @export
 addNewPage <- function(id, name, index) {
   method <- "addNewPage"
   callJS()
 }
 
 
+#' @export
 addNewQuestion <- function(id, questionType, name, index) {
   method <- "addNewQuestion"
   callJS()
 }
 
 
+#' @export
 title <- function(id, title) {
   method <- "title"
   callJS()
 }
 
 
+#' @export
 name <- function(id, name) {
   method <- "name"
   callJS()
 }
 
 
+#' @export
 description <- function(id, description) {
   method <- "description"
   callJS()
 }
 
 
+#' @export
 isRequired <- function(id) {
   method <- "isRequired"
   callJS()
 }
 
 
+#' @export
 requiredErrorText <- function(id, requiredErrorText) {
   method <- "requiredErrorText"
   callJS()
 }
 
 
+#' @export
 placeHolder <- function(id, placeHolder) {
   method <- "placeHolder"
   callJS()
 }
 
 
+#' @export
 visibleIf <- function(id, expression) {
   method <- "visibleIf"
   callJS()
 }
 
 
+#' @export
 requiredIf <- function(id, expression) {
   method <- "requiredIf"
   callJS()
 }
 
 
+#' @export
 cssClasses <- function(id) {
   method <- "cssClasses"
   callJS()
 }
 
 
+#' @export
 defaultValue <- function(id, defaultValue) {
   method <- "defaultValue"
   callJS()
 }
 
 
+#' @export
 inputType <- function(id, inputType = c("color", "date", "datetime", "datetime-local", "email", "month", "number", "password", "range", "tel", "text", "time", "url", "week")) {
   inputType <- match.arg(inputType)
   method <- "inputType"
@@ -104,24 +121,28 @@ inputType <- function(id, inputType = c("color", "date", "datetime", "datetime-l
 }
 
 
+#' @export
 max <- function(id, max) {
   method <- "max"
   callJS()
 }
 
 
+#' @export
 maxErrorText <- function(id, maxErrorText) {
   method <- "maxErrorText"
   callJS()
 }
 
 
+#' @export
 min <- function(id, min) {
   method <- "min"
   callJS()
 }
 
 
+#' @export
 minErrorText <- function(id, minErrorText) {
   method <- "minErrorText"
   callJS()
@@ -158,78 +179,91 @@ choices <- function(id, choices) {
 }
 
 
+#' @export
 noneText <- function(id, noneText) {
   method <- "noneText"
   callJS()
 }
 
 
+#' @export
 otherPlaceHolder <- function(id, otherPlaceHolder) {
   method <- "otherPlaceHolder"
   callJS()
 }
 
 
+#' @export
 otherText <- function(id, otherText) {
   method <- "otherText"
   callJS()
 }
 
 
+#' @export
 getAllQuestions <- function(id) {
   method <- "getAllQuestions"
   callJS()
 }
 
 
+#' @export
 html <- function(id, html) {
   method <- "html"
   callJS()
 }
 
 
+#' @export
 toJSON <- function(id) {
   method <- "toJSON"
   callJS()
 }
 
 
+#' @export
 setValue <- function(id, name, value) {
   method <- "setValue"
   callJS()
 }
 
 
+#' @export
 nextPage <- function(id) {
   method <- "nextPage"
   callJS()
 }
 
 
+#' @export
 answersOnComplete <- function(id) {
   method <- "answersOnComplete"
   callJS()
 }
 
 
+#' @export
 doComplete <- function(id) {
   method <- "doComplete"
   callJS()
 }
 
 
+#' @export
 onValueChanged <- function(id, name = NULL) {
   method <- "onValueChanged"
   callJS()
 }
 
 
+#' @export
 completedHtml <- function(id, completedHtml) {
   method <- "completedHtml"
   callJS()
 }
 
 
+#' @export
 showCompletedPage <- function(id, showCompletedPage) {
   method <- "showCompletedPage"
   callJS()
